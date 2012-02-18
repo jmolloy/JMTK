12f57d6dc7a3010a2988a212cb389f2b33c86484
James Molloy <james@jamesmolloy.co.uk>
2012-02-18 21:09:46 +0000

    Remove build dir

--- src/build/CMakeFiles/floppy.img.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/floppy.img"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/floppy.img.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
